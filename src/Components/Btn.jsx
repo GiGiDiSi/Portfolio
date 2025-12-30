4cd6d3e6f21097496755d0063945f9d45ffed557
GiGiDiSi <subs.ggds@gmail.com>
2025-12-30 23:01:48 +0800

    first commit

--- src/Components/Btn.jsx
+++ src/Components/Btn.jsx
@@ -4,31 +4,31 @@ import '../Styles/Btns.css'
 function Btn(){
     return(
         <div className = "container">
-            <a href="/Original">
+            <a href="Original">
                 <button className='btn'>
                     <h2>Original</h2>
                 </button>
             </a>
 
-            <a href="/FanArts">
+            <a href="FanArts">
                 <button className='btn'>
                     <h2>Fan Arts</h2>
                 </button>
             </a>
 
-            <a href="/Animation">
+            <a href="Animation">
                 <button className='btn'>
                     <h2>Animation</h2>
                 </button>
             </a>
 
-            <a href = "/About">
+            <a href = "About">
                 <button className='btn'>
                     <h2>About</h2>
                 </button>
             </a>
 
-            <a href = "/FAQ">
+            <a href = "FAQ">
                 <button className='btn'>
                     <h2>FAQ</h2>
                 </button>
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,23 +6,23 @@ const Sidebar = ({ isOpen }) => {
     <div className= {`${isOpen ? "modal" : ""}`} >
       <div className={`sidebar ${isOpen ? "open" : "closed"}`}>
         <div className="container">
-          <a href="/Original">
+          <a href="Original">
             <h2>Original</h2>
           </a>
 
-          <a href="/FanArts">
+          <a href="FanArts">
             <h2>Fan Arts</h2>
           </a>
 
-          <a href="/Animation">
+          <a href="Animation">
             <h2>Animation</h2>
           </a>
 
-          <a href="/About">
+          <a href="About">
             <h2>About</h2>
           </a>
 
-          <a href="/FAQ">
+          <a href="FAQ">
             <h2>FAQ</h2>
           </a>
           <Socials />
